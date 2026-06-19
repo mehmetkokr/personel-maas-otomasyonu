9dd0b0a80a5093f582a0d3b24fc502f7bdddae17
mehmetkokr <mehmetk.okr13@gmail.com>
2026-06-19 03:25:02 +0300

    Proje Dosyaları

---
+++ PersonelMaasTakipSistemi/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace PersonelMaasTakipSistemi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtDepartman = new System.Windows.Forms.TextBox();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.txtMesai = new System.Windows.Forms.TextBox();
+            this.txtPrim = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Personel No :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Ad Soyad :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Departman :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Temel Maaş :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Mesai Saati :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Prim :";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(150, 17);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(150, 22);
+            this.txtNo.TabIndex = 11;
+            this.txtNo.Text = "hmet";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(150, 52);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(150, 22);
+            this.txtAdSoyad.TabIndex = 10;
+            // 
+            // txtDepartman
+            // 
+            this.txtDepartman.Location = new System.Drawing.Point(150, 87);
+            this.txtDepartman.Name = "txtDepartman";
+            this.txtDepartman.Size = new System.Drawing.Size(150, 22);
+            this.txtDepartman.TabIndex = 9;
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(150, 122);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(150, 22);
+            this.txtMaas.TabIndex = 8;
+            // 
+            // txtMesai
+            // 
+            this.txtMesai.Location = new System.Drawing.Point(150, 157);
+            this.txtMesai.Name = "txtMesai";
+            this.txtMesai.Size = new System.Drawing.Size(150, 22);
+            this.txtMesai.TabIndex = 7;
+            // 
+            // txtPrim
+            // 
+            this.txtPrim.Location = new System.Drawing.Point(150, 192);
+            this.txtPrim.Name = "txtPrim";
+            this.txtPrim.Size = new System.Drawing.Size(150, 22);
+            this.txtPrim.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.ForeColor = System.Drawing.Color.White;
+            this.btnEkle.Location = new System.Drawing.Point(340, 17);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(90, 30);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "➕ Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(255)))));
+            this.btnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuncelle.ForeColor = System.Drawing.Color.White;
+            this.btnGuncelle.Location = new System.Drawing.Point(340, 55);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(90, 30);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "✏ Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(53)))), ((int)(((byte)(69)))));
+            this.btnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSil.ForeColor = System.Drawing.Color.White;
+            this.btnSil.Location = new System.Drawing.Point(340, 93);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(90, 30);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "🗑 Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(162)))), ((int)(((byte)(184)))));
+            this.btnHesapla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHesapla.ForeColor = System.Drawing.Color.White;
+            this.btnHesapla.Location = new System.Drawing.Point(340, 131);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(90, 30);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "🧮 Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(117)))), ((int)(((byte)(125)))));
+            this.btnTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTemizle.ForeColor = System.Drawing.Color.White;
+            this.btnTemizle.Location = new System.Drawing.Point(340, 169);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(90, 30);
+            this.btnTemizle.TabIndex = 1;
+            this.btnTemizle.Text = "🧹 Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 240);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(750, 180);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtPrim);
+            this.Controls.Add(this.txtMesai);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.txtDepartman);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Personel Maaş Takip Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtDepartman;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.TextBox txtMesai;
+        private System.Windows.Forms.TextBox txtPrim;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ PersonelMaasTakipSistemi/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PersonelMaasTakipSistemi
+{
+    public partial class Form1 : Form
+    {
+        public class Personel
+        {
+            public string No { get; set; }
+            public string AdSoyad { get; set; }
+            public string Departman { get; set; }
+            public double Maas { get; set; }
+            public int Mesai { get; set; }
+            public double Prim { get; set; }
+            public double ToplamMaas { get; set; }
+        }
+
+        List<Personel> personeller = new List<Personel>();
+        int seciliIndex = -1; // Seçili satırın indexi
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sütunları manuel olarak tanımla
+            dataGridView1.Columns.Add("No", "Personel No");
+            dataGridView1.Columns.Add("AdSoyad", "Ad Soyad");
+            dataGridView1.Columns.Add("Departman", "Departman");
+            dataGridView1.Columns.Add("Maas", "Temel Maaş");
+            dataGridView1.Columns.Add("Mesai", "Mesai Saati");
+            dataGridView1.Columns.Add("Prim", "Prim");
+            dataGridView1.Columns.Add("ToplamMaas", "Toplam Maaş");
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        // Grid'i listeden yeniden doldur
+        private void GridiYenile()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (Personel p in personeller)
+            {
+                dataGridView1.Rows.Add(
+                    p.No,
+                    p.AdSoyad,
+                    p.Departman,
+                    p.Maas.ToString("F2") + " TL",
+                    p.Mesai + " saat",
+                    p.Prim.ToString("F2") + " TL",
+                    p.ToplamMaas.ToString("F2") + " TL"
+                );
+            }
+            seciliIndex = -1;
+        }
+
+        // Satıra tıklayınca textbox'lara doldur
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.RowIndex >= personeller.Count) return;
+
+            seciliIndex = e.RowIndex;
+            Personel p = personeller[seciliIndex];
+
+            txtNo.Text = p.No;
+            txtAdSoyad.Text = p.AdSoyad;
+            txtDepartman.Text = p.Departman;
+            txtMaas.Text = p.Maas.ToString();
+            txtMesai.Text = p.Mesai.ToString();
+            txtPrim.Text = p.Prim.ToString();
+        }
+
+        // Sayısal alanları kontrol eden yardımcı metot
+        private bool NotkontrolEt(out double maas, out int mesai, out double prim)
+        {
+            maas = 0; mesai = 0; prim = 0;
+
+            if (!double.TryParse(txtMaas.Text, out maas) || maas < 0)
+            {
+                MessageBox.Show("Maaş geçerli bir sayı olmalıdır!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (!int.TryParse(txtMesai.Text, out mesai) || mesai < 0)
+            {
+                MessageBox.Show("Mesai geçerli bir tam sayı olmalıdır!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (!double.TryParse(txtPrim.Text, out prim) || prim < 0)
+            {
+                MessageBox.Show("Prim geçerli bir sayı olmalıdır!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        // EKLE
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNo.Text) ||
+                string.IsNullOrWhiteSpace(txtAdSoyad.Text) ||
+                string.IsNullOrWhiteSpace(txtDepartman.Text))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            double maas, prim;
+            int mesai;
+            if (!NotkontrolEt(out maas, out mesai, out prim)) return;
+
+            Personel p = new Personel();
+            p.No = txtNo.Text.Trim();
+            p.AdSoyad = txtAdSoyad.Text.Trim();
+            p.Departman = txtDepartman.Text.Trim();
+            p.Maas = maas;
+            p.Mesai = mesai;
+            p.Prim = prim;
+            p.ToplamMaas = p.Maas + (p.Mesai * 200) + p.Prim;
+
+            personeller.Add(p);
+            GridiYenile();
+            AlanlariTemizle();
+            MessageBox.Show("Personel eklendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // SİL
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (seciliIndex < 0 || seciliIndex >= personeller.Count)
+            {
+                MessageBox.Show("Lütfen önce bir satır seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult sonuc = MessageBox.Show("Bu personeli silmek istiyor musunuz?", "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sonuc == DialogResult.Yes)
+            {
+                personeller.RemoveAt(seciliIndex);
+                GridiYenile();
+                AlanlariTemizle();
+            }
+        }
+
+        // GÜNCELLE
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (seciliIndex < 0 || seciliIndex >= personeller.Count)
+            {
+                MessageBox.Show("Lütfen önce bir satır seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            double maas, prim;
+            int mesai;
+            if (!NotkontrolEt(out maas, out mesai, out prim)) return;
+
+            Personel p = personeller[seciliIndex];
+            p.No = txtNo.Text.Trim();
+            p.AdSoyad = txtAdSoyad.Text.Trim();
+            p.Departman = txtDepartman.Text.Trim();
+            p.Maas = maas;
+            p.Mesai = mesai;
+            p.Prim = prim;
+            p.ToplamMaas = p.Maas + (p.Mesai * 200) + p.Prim;
+
+            GridiYenile();
+            AlanlariTemizle();
+            MessageBox.Show("Personel güncellendi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // HESAPLA
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double maas, prim;
+            int mesai;
+            if (!NotkontrolEt(out maas, out mesai, out prim)) return;
+
+            double toplam = maas + (mesai * 200) + prim;
+            MessageBox.Show(
+                "Temel Maaş     : " + maas + " TL\n" +
+                "Mesai (" + mesai + " x 200) : " + (mesai * 200) + " TL\n" +
+                "Prim           : " + prim + " TL\n" +
+                "─────────────────────\n" +
+                "Toplam Maaş    : " + toplam + " TL",
+                "Maaş Hesaplama",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        // TEMİZLE
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            AlanlariTemizle();
+        }
+
+        private void AlanlariTemizle()
+        {
+            txtNo.Text = "";
+            txtAdSoyad.Text = "";
+            txtDepartman.Text = "";
+            txtMaas.Text = "";
+            txtMesai.Text = "";
+            txtPrim.Text = "";
+            seciliIndex = -1;
+        }
+    }
+}
